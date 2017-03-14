aa5d85e3c8eb3d15fd76607c41935d907683b426
stijnvanhoey <stijnvanhoey@gmail.com>
2017-03-14 14:22:23 +0100

    add first draft example of some functions

---
+++ pydov/example1.py
@@ -0,0 +1,32 @@
+
+import matplotlib.pyplot as plt
+from dovseries import DovGroundwater
+
+
+dov = DovGroundwater("../exampleXML/1-0709.xml")
+dov.peilmetingen.plot()
+
+dov.peilmetingen_timeseries.resample('A').mean().plot()
+
+print(dov.peilmetingen.head())
+print(dov.observaties.head())
+print(dov.metadata_locatie)
+print(dov.metadata_filters['1'])
+print(dov.peilmetingen.shape)
+
+
+dov.peilmetingen.reset_index().pivot_table(index="datum",
+                                           columns=["grondwaterlocatie",
+                                                    "filternummer"],
+                                           values="diepte").plot()
+
+
+dov.observaties.pivot_table(index="datum",
+                            columns=["parameter"],
+                            values="waarde").plot()
+
+
+dov.get_parameter_series("NO3").plot(kind='barh')
+plt.tight_layout()
+
+plt.show()

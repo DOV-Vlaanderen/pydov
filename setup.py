1b9a1a2c59e62e841e2521aa48f072e931e2bb99
Johan Van de Wauw <Johan.VandeWauw@realdolmen.com>
2017-03-15 09:07:42 +0100

    Small fixes to setup script

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(
     name='pydov',
     version='0.1.0',
     description="A python package to retrieve data from Databank Ondergrond Vlaanderen (DOV)",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author="DOV-Vlaanderen",
-    author_email='stijnvanhoey@gmail.com',
+    author_email='dov@vlaanderen.be',
     url='https://github.com/DOV-Vlaanderen/pydov',
     packages=[
         'pydov',
@@ -20,7 +20,10 @@ setup(
     package_dir={'pydov':
                  'pydov'},
     include_package_data=True,
-    install_requires=['pandas'],
+    install_requires=[
+        'pandas',
+        'xmltodict'
+    ],
     license="MIT license",
     zip_safe=False,
     keywords='pydov',
@@ -33,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=['pandas']

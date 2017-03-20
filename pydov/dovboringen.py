2831362ee609a3df268dae1677e9379527673be6
Pieter Jan Haest <geo.haest@gmail.com>
2017-03-20 11:05:51 +0100

    Add wfs server variables

--- pydov/dovboringen.py
+++ pydov/dovboringen.py
@@ -26,9 +26,48 @@ class DOVVariableError(Exception):
 
 class DovBoringen(object):
 
-    def __init__(self, ):
+    def __init__(self, url='https://www.dov.vlaanderen.be/geoserver/wfs', version='2.0.0', layer='dov-pub:Boringen',
+                 maxfeatures=10000, timeout=30, outputformat='xml', epsg='31370'):
         """ Initialize DovBoringen instance to read borehole data for selected locations in xml
+
+        Parameters
+        ----------
+        url: str
+            url string
+        version: str
+            the version on the wfs_boring
+        layer: str
+            the layer with the general borehole data
+        maxfeatures: int
+            the maximum number of features that will be obtained from the wfs_boring
+        timeout: int
+            time in seconds after which requests should time-out
+        outputformat: str
+            the format that is returned from the wfs_boring
+        epsg: int
+            the epsg code in which data should be retrieved. Default value for Lamber72: 31370
         """
+        # define general wfs_boring info
+        self.layer = layer
+        self.outputformat = 'application/'+outputformat
+        self.timeout = timeout
+        self.wfs_boring = WebFeatureService(url=url, version=version)
+        # check version
+        self.version = self.wfs_boring.identification.version
+        # check contents through: self.wfs_boring.contents['dov-pub:Boringen']
+        self.maxfeatbool = False    # a boolean indicating that a limit exists on the queryable features
+        self.srsname = 'urn:x-ogc:def:crs:EPSG:'+epsg
+
+        try:
+            server_maxfeatures = int(self.wfs_boring.constraints['DefaultMaxFeatures'].values[0])
+            if server_maxfeatures < maxfeatures:
+                self.maxfeatures = server_maxfeatures
+                self.maxfeatbool = True
+            else:
+                self.maxfeatures = maxfeatures
+        except:
+            self.maxfeatures = maxfeatures
+
 
 
 if __name__ == '__main__':

968372dca05f130b382ddef9e0712efcf5ac33a7
Roel Huybrechts <roel.huybrechts@realdolmen.com>
2018-01-12 11:58:34 +0100

    Add docs to owsutil tests.

--- tests/test_util_owsutil.py
+++ tests/test_util_owsutil.py
@@ -5,7 +5,6 @@ import sys
 import owslib
 import pytest
 from numpy.compat import unicode
-
 from owslib.etree import etree
 from owslib.iso import MD_Metadata
 from owslib.util import nspath_eval
@@ -20,6 +19,21 @@ from pydov.util.errors import (
 
 @pytest.fixture
 def wfs(monkeypatch):
+    """PyTest fixture providing an instance of a WebFeatureService based on
+    a local copy of a GetCapabilities request.
+
+    Parameters
+    ----------
+    monkeypatch : pytest.fixture
+        PyTest monkeypatch fixture.
+
+    Returns
+    -------
+    owslib.wfs.WebFeatureService
+        WebFeatureService based on the local GetCapabilities.
+
+    """
+
     def read(*args, **kwargs):
         with open('tests/data/util/owsutil/wfscapabilities.xml', 'r') as f:
             data = f.read()
@@ -38,6 +52,24 @@ def wfs(monkeypatch):
 
 @pytest.fixture
 def md_metadata(wfs, monkeypatch):
+    """PyTest fixture providing a MD_Metadata instance of the
+    dov-pub:Boringen layer.
+
+    Parameters
+    ----------
+    wfs : pytest.fixture returning owslib.wfs.WebFeatureService
+        WebFeatureService based on the local GetCapabilities.
+    monkeypatch : pytest.fixture
+        PyTest monkeypatch fixture.
+
+    Returns
+    -------
+    owslib.iso.MD_Metadata
+        Parsed metadata describing the Boringen WFS layer in more detail,
+        in the ISO 19115/19139 format.
+
+    """
+
     def openURL(*args, **kwargs):
         class Io:
             def read(*args, **kwargs):
@@ -46,6 +78,7 @@ def md_metadata(wfs, monkeypatch):
                     if type(data) is not bytes:
                         data = data.encode('utf-8')
                 return data
+
         return Io()
 
     if sys.version_info[0] < 3:
@@ -59,6 +92,16 @@ def md_metadata(wfs, monkeypatch):
 
 @pytest.fixture
 def mp_remote_fc(monkeypatch):
+    """Monkeypatch the call to get the remote feature catalogue of the
+    dov-pub:Boringen layer.
+
+    Parameters
+    ----------
+    monkeypatch : pytest.fixture
+        PyTest monkeypatch fixture.
+
+    """
+
     def openURL(*args, **kwargs):
         class Io:
             def read(*args, **kwargs):
@@ -69,6 +112,7 @@ def mp_remote_fc(monkeypatch):
                     if type(data) is not bytes:
                         data = data.encode('utf-8')
                 return data
+
         return Io()
 
     if sys.version_info[0] < 3:
@@ -79,6 +123,15 @@ def mp_remote_fc(monkeypatch):
 
 @pytest.fixture
 def mp_remote_fc_notfound(monkeypatch):
+    """Monkeypatch the call to get an inexistent remote featurecatalogue.
+
+    Parameters
+    ----------
+    monkeypatch : pytest.fixture
+        PyTest monkeypatch fixture.
+
+    """
+
     def openURL(*args, **kwargs):
         class Io:
             def read(*args, **kwargs):
@@ -89,6 +142,7 @@ def mp_remote_fc_notfound(monkeypatch):
                     if type(data) is not bytes:
                         data = data.encode('utf-8')
                 return data
+
         return Io()
 
     if sys.version_info[0] < 3:
@@ -96,8 +150,18 @@ def mp_remote_fc_notfound(monkeypatch):
     else:
         monkeypatch.setattr('owslib.util.openURL.__code__', openURL.__code__)
 
+
 @pytest.fixture
 def mp_remote_describefeaturetype(monkeypatch):
+    """Monkeypatch the call to a remote DescribeFeatureType of the
+    dov-pub:Boringen layer.
+
+    Parameters
+    ----------
+    monkeypatch : pytest.fixture
+        PyTest monkeypatch fixture.
+
+    """
     def openURL(*args, **kwargs):
         class Io:
             def read(*args, **kwargs):
@@ -108,6 +172,7 @@ def mp_remote_describefeaturetype(monkeypatch):
                     if type(data) is not bytes:
                         data = data.encode('utf-8')
                 return data
+
         return Io()
 
     if sys.version_info[0] < 3:
@@ -117,22 +182,69 @@ def mp_remote_describefeaturetype(monkeypatch):
 
 
 class TestOwsutil(object):
+    """Class grouping tests for the pydov.util.owsutil module."""
+
     def test_get_csw_base_url(self, wfs):
+        """Test the owsutil.get_csw_base_url method.
+
+        Test whether the CSW base URL of the dov-pub:Boringen layer is correct.
+
+        Parameters
+        ----------
+        wfs : pytest.fixture returning owslib.wfs.WebFeatureService
+            WebFeatureService based on the local GetCapabilities.
+
+        """
         contentmetadata = wfs.contents['dov-pub:Boringen']
         assert owsutil.get_csw_base_url(contentmetadata) == \
                'https://www.dov.vlaanderen.be/geonetwork/srv/nl/csw'
 
     def test_get_csw_base_url_nometadataurls(self, wfs):
+        """Test the owsutil.get_csw_base_url method for a layer without
+        metdata urls.
+
+        Test whether a MetadataNotFoundError is raised.
+
+        Parameters
+        ----------
+        wfs : pytest.fixture returning owslib.wfs.WebFeatureService
+            WebFeatureService based on the local GetCapabilities.
+
+        """
         contentmetadata = wfs.contents['dov-pub:Boringen']
         contentmetadata.metadataUrls = []
         with pytest.raises(MetadataNotFoundError):
             owsutil.get_csw_base_url(contentmetadata)
 
     def test_get_featurecatalogue_uuid(self, md_metadata):
+        """Test the owsutil.get_featurecatalogue_uuid method.
+
+        Test whether the featurecatalogue uuid of the dov-pub:Boringen layer
+        is correct.
+
+        Parameters
+        ----------
+        md_metadata : pytest.fixture providing owslib.iso.MD_Metadata
+            Parsed metadata describing the Boringen WFS layer in more detail,
+            in the ISO 19115/19139 format.
+
+        """
         assert owsutil.get_featurecatalogue_uuid(md_metadata) == \
                'c0cbd397-520f-4ee1-aca7-d70e271eeed6'
 
     def test_get_featurecatalogue_uuid_nocontentinfo(self, md_metadata):
+        """Test the owsutil.get_featurecatalogue_uuid method when the
+        metadata is missing the gmd:contentInfo element.
+
+        Test whether a FeatureCatalogueNotFoundError is raised.
+
+        Parameters
+        ----------
+        md_metadata : pytest.fixture providing owslib.iso.MD_Metadata
+            Parsed metadata describing the Boringen WFS layer in more detail,
+            in the ISO 19115/19139 format.
+
+        """
         tree = etree.fromstring(md_metadata.xml)
         root = tree.find('{http://www.isotc211.org/2005/gmd}MD_Metadata')
         for ci in tree.findall(
@@ -144,12 +256,24 @@ class TestOwsutil(object):
             owsutil.get_featurecatalogue_uuid(md_metadata)
 
     def test_get_featurecatalogue_uuid_nouuidref(self, md_metadata):
+        """Test the owsutil.get_featurecatalogue_uuid method when the
+        gmd:contentInfo element is missing a 'uuidref' attribute.
+
+        Test whether a FeatureCatalogueNotFoundError is raised.
+
+        Parameters
+        ----------
+        md_metadata : pytest.fixture providing owslib.iso.MD_Metadata
+            Parsed metadata describing the Boringen WFS layer in more detail,
+            in the ISO 19115/19139 format.
+
+        """
         tree = etree.fromstring(md_metadata.xml)
         for ci in tree.findall(nspath_eval(
-                'gmd:MD_Metadata/gmd:contentInfo/'
-                'gmd:MD_FeatureCatalogueDescription/'
-                'gmd:featureCatalogueCitation',
-                {'gmd': 'http://www.isotc211.org/2005/gmd'})):
+            'gmd:MD_Metadata/gmd:contentInfo/'
+            'gmd:MD_FeatureCatalogueDescription/'
+            'gmd:featureCatalogueCitation',
+            {'gmd': 'http://www.isotc211.org/2005/gmd'})):
             ci.attrib.pop('uuidref')
         md_metadata.xml = etree.tostring(tree)
 
@@ -157,10 +281,35 @@ class TestOwsutil(object):
             owsutil.get_featurecatalogue_uuid(md_metadata)
 
     def test_get_namespace(self, wfs, mp_remote_describefeaturetype):
+        """Test the owsutil.get_namespace method.
+
+        Test whether the namespace of the dov-pub:Boringen layer is correct.
+
+        Parameters
+        ----------
+        wfs : pytest.fixture returning owslib.wfs.WebFeatureService
+            WebFeatureService based on the local GetCapabilities.
+        mp_remote_describefeaturetype : pytest.fixture
+            Monkeypatch the call to a remote DescribeFeatureType of the
+            dov-pub:Boringen layer.
+
+        """
         assert owsutil.get_namespace(wfs, 'dov-pub:Boringen') == \
                'http://dov.vlaanderen.be/ocdov/dov-pub'
 
     def test_get_remote_featurecatalogue(self, mp_remote_fc):
+        """Test the owsutil.get_remote_featurecatalogue method.
+
+        Test whether the feature catalogue of the dov-pub:Boringen layer
+        matches the format described in the docs.
+
+        Parameters
+        ----------
+        mp_remote_fc : pytest.fixture
+            Monkeypatch the call to get the remote feature catalogue of the
+            dov-pub:Boringen layer.
+
+        """
         fc = owsutil.get_remote_featurecatalogue(
             'https://www.dov.vlaanderen.be/geonetwork/srv/nl/csw',
             'c0cbd397-520f-4ee1-aca7-d70e271eeed6')
@@ -196,15 +345,48 @@ class TestOwsutil(object):
                 assert (type(mp[1]) is int and mp[1] > 0) or mp[1] == 'Inf'
 
     def test_get_remote_featurecataloge_baduuid(self, mp_remote_fc_notfound):
+        """Test the owsutil.get_remote_featurecatalogue method with an
+        inexistent feature catalogue uuid.
+
+        Test whether a FeatureCatalogueNotFoundError is raised.
+
+        Parameters
+        ----------
+        mp_remote_fc_notfound : pytest.fixture
+            Monkeypatch the call to get an inexistent remote featurecatalogue.
+
+        """
         with pytest.raises(FeatureCatalogueNotFoundError):
             owsutil.get_remote_featurecatalogue(
                 'https://www.dov.vlaanderen.be/geonetwork/srv/nl/csw',
                 'badfc000-0000-0000-0000-badfc00badfc')
 
     def test_get_remote_metadata(self, md_metadata):
+        """Test the owsutil.get_remote_metadata method.
+
+        Test whether the resulting MD_Metadata is correct.
+
+        Parameters
+        ----------
+        md_metadata : pytest.fixture returning owslib.iso.MD_Metadata
+            Parsed metadata describing the Boringen WFS layer in more detail,
+            in the ISO 19115/19139 format.
+
+        """
         assert type(md_metadata) is MD_Metadata
 
-    def test_get_remote_metadata_exception(self, wfs):
+    def test_get_remote_metadata_nometadataurls(self, wfs):
+        """Test the owsutil.get_remote_metadata method when the WFS layer
+        missed metadata URLs.
+
+        Test whether a MetadataNotFoundError is raised.
+
+        Parameters
+        ----------
+        wfs : pytest.fixture returning owslib.wfs.WebFeatureService
+            WebFeatureService based on the local GetCapabilities.
+
+        """
         contentmetadata = wfs.contents['dov-pub:Boringen']
         contentmetadata.metadataUrls = []
         with pytest.raises(MetadataNotFoundError):

66b8c646fa67bd170fb3cf806212fda360b9be3d
pjhaest <geo.haest@gmail.com>
2018-05-04 15:34:33 +0200

    add first code of hydrogeologische stratigrafie (copy of informele stratigrafie)

--- pydov/search/interpretaties.py
+++ pydov/search/interpretaties.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from pydov.search.abstract import AbstractSearch
 from pydov.types.interpretaties import InformeleStratigrafie
+from pydov.types.interpretaties import HydrogeologischeStratigrafie
 from pydov.util import owsutil
 
 
@@ -22,6 +23,9 @@ class InformeleStratigrafieSearch(AbstractSearch):
         """Initialise the WFS namespace associated with the layer."""
         if InformeleStratigrafieSearch.__wfs_namespace is None:
             InformeleStratigrafieSearch.__wfs_namespace = self._get_namespace()
+        if HydrogeologischeStratigrafieSearch.__wfs_namespace is None:
+            HydrogeologischeStratigrafieSearch.__wfs_namespace = \
+                self._get_namespace()
 
     def _init_fields(self):
         """Initialise the fields and their metadata available in this search
@@ -118,4 +122,123 @@ class InformeleStratigrafieSearch(AbstractSearch):
             data=InformeleStratigrafie.to_df_array(
                 interpretaties, return_fields),
             columns=InformeleStratigrafie.get_field_names(return_fields))
+        return df
+
+
+class HydrogeologischeStratigrafieSearch(AbstractSearch):
+    """Search class to retrieve hydrogeological interpretations """
+
+    __wfs_schema = None
+    __wfs_namespace = None
+    __md_metadata = None
+    __fc_featurecatalogue = None
+
+    def __init__(self):
+        """Initialisation."""
+        super(HydrogeologischeStratigrafieSearch, self).__init__(
+            'interpretaties:hydrogeologische_stratigrafie',
+            HydrogeologischeStratigrafie)
+
+    def _init_namespace(self):
+        """Initialise the WFS namespace associated with the layer."""
+        if HydrogeologischeStratigrafieSearch.__wfs_namespace is None:
+            HydrogeologischeStratigrafieSearch.__wfs_namespace = \
+                self._get_namespace()
+
+    def _init_fields(self):
+        """Initialise the fields and their metadata available in this search
+        class."""
+        if self._fields is None:
+            if HydrogeologischeStratigrafieSearch.__wfs_schema is None:
+                HydrogeologischeStratigrafieSearch.__wfs_schema = \
+                    self._get_schema()
+
+            if HydrogeologischeStratigrafieSearch.__md_metadata is None:
+                HydrogeologischeStratigrafieSearch.__md_metadata = \
+                    self._get_remote_metadata()
+
+            if HydrogeologischeStratigrafieSearch.__fc_featurecatalogue is None:
+                csw_url = self._get_csw_base_url()
+                fc_uuid = owsutil.get_featurecatalogue_uuid(
+                    HydrogeologischeStratigrafieSearch.__md_metadata)
+
+                HydrogeologischeStratigrafieSearch.__fc_featurecatalogue = \
+                    owsutil.get_remote_featurecatalogue(csw_url, fc_uuid)
+
+            fields = self._build_fields(
+                HydrogeologischeStratigrafieSearch.__wfs_schema,
+                HydrogeologischeStratigrafieSearch.__fc_featurecatalogue)
+
+            for field in fields.values():
+                if field['name'] not in self._type.get_field_names(
+                        include_wfs_injected=True):
+                    self._type._fields.append({
+                        'name': field['name'],
+                        'source': 'wfs',
+                        'sourcefield': field['name'],
+                        'type': field['type'],
+                        'wfs_injected': True
+                    })
+
+            self._fields = self._build_fields(
+                HydrogeologischeStratigrafieSearch.__wfs_schema,
+                HydrogeologischeStratigrafieSearch.__fc_featurecatalogue)
+
+    def search(self, location=None, query=None, return_fields=None):
+        """Search for boreholes (Boring). Provide either `location` or `query`.
+        When `return_fields` is None, all fields are returned.
+
+        Parameters
+        ----------
+        location : tuple<minx,miny,maxx,maxy>
+            The bounding box limiting the features to retrieve.
+        query : owslib.fes.OgcExpression
+            OGC filter expression to use for searching. This can contain any
+            combination of filter elements defined in owslib.fes. The query
+            should use the fields provided in `get_fields()`. Note that not
+            all fields are currently supported as a search parameter.
+        return_fields : list<str> or tuple<str> or set<str>
+            A list of fields to be returned in the output data. This should
+            be a subset of the fields provided in `get_fields()`. Note that
+            not all fields are currently supported as return fields.
+
+        Returns
+        -------
+        pandas.core.frame.DataFrame
+            DataFrame containing the output of the search query.
+
+        Raises
+        ------
+        pydov.util.errors.InvalidSearchParameterError
+            When not one of `location` or `query` is provided.
+
+        pydov.util.errors.InvalidFieldError
+            When at least one of the fields in `return_fields` is unknown.
+
+            When a field that is only accessible as return field is used as
+            a query parameter.
+
+            When a field that can only be used as a query parameter is used as
+            a return field.
+
+        pydov.util.errors.FeatureOverflowError
+            When the number of features to be returned is equal to the
+            maxFeatures limit of the WFS server.
+
+        AttributeError
+            When the argument supplied as return_fields is not a list,
+            tuple or set.
+
+        """
+        fts = self._search(location=location, query=query,
+                           return_fields=return_fields,
+                           extra_wfs_fields=['Type_proef', 'Proeffiche'])
+
+        interpretaties = HydrogeologischeStratigrafie.from_wfs(
+            fts, self.__wfs_namespace)
+
+        df = pd.DataFrame(
+            data=HydrogeologischeStratigrafie.to_df_array(
+                interpretaties, return_fields),
+            columns=HydrogeologischeStratigrafie.get_field_names(return_fields))
         return df
--- pydov/types/interpretaties.py
+++ pydov/types/interpretaties.py
@@ -193,3 +193,188 @@ class InformeleStratigrafie(AbstractDovType):
             )
 
         self._parse_subtypes(xml)
+
+
+class HydrogeologischeStratigrafieLaag(AbstractDovSubType):
+
+    _name = 'hydrogeologische_stratigrafie_laag'
+    _rootpath = './/hydrogeologischestratigrafie/laag'
+
+    _fields = [{
+        'name': 'diepte_laag_van',
+        'source': 'xml',
+        'sourcefield': '/van',
+        'definition': 'Diepte van de bovenkant van de laag hydrogeologische '
+                      'stratigrafie in meter.',
+        'type': 'float',
+        'notnull': False
+    }, {
+        'name': 'diepte_laag_tot',
+        'source': 'xml',
+        'sourcefield': '/tot',
+        'definition': 'Diepte van de onderkant van de laag hydrogeologische '
+                      'stratigrafie in meter.',
+        'type': 'float',
+        'notnull': False
+    }, {
+        'name': 'aquifer',
+        'source': 'xml',
+        'sourcefield': '/aquifer',
+        'definition': 'code van de watervoerende laag waarin de laag '
+                      'Hydrogeologische stratigrafie zich bevindt.',
+        'type': 'string',
+        'notnull': False
+    }]
+
+    def __init__(self):
+        """Initialisation."""
+        super(HydrogeologischeStratigrafieLaag, self).__init__(
+            'hydrogeologische_stratigrafie_laag')
+
+    @classmethod
+    def from_xml_element(cls, element):
+        """Build an instance of this subtype from a single XML element.
+
+        Parameters
+        ----------
+        element : etree.Element
+            XML element representing a single record of this subtype.
+
+        """
+        laag = InformeleStratigrafieLaag()
+
+        for field in cls.get_fields().values():
+            laag.data[field['name']] = laag._parse(
+                func=element.findtext,
+                xpath=field['sourcefield'],
+                namespace=None,
+                returntype=field.get('type', None)
+            )
+
+        return laag
+
+
+class HydrogeologischeStratigrafie(AbstractDovType):
+    """Class representing the DOV data type for boreholes."""
+
+    _subtypes = [HydrogeologischeStratigrafieLaag]
+
+    _fields = [{
+        'name': 'pkey_interpretatie',
+        'source': 'wfs',
+        'sourcefield': 'Interpretatiefiche',
+        'type': 'string'
+    }, {
+        'name': 'pkey_boring',
+        'source': 'custom',
+        'type': 'string',
+        'definition': 'URL die verwijst naar de gegevens van de boring '
+                      'waaraan deze hydrogeologische stratigrafie gekoppeld is '
+                      '(indien gekoppeld aan een boring).',
+        'notnull': False
+    }, {
+        'name': 'pkey_sondering',
+        'source': 'custom',
+        'type': 'string',
+        'definition': 'URL die verwijst naar de gegevens van de sondering '
+                      'waaraan deze informele stratigrafie gekoppeld is ('
+                      'indien gekoppeld aan een sondering).',
+        'notnull': False
+    }, {
+        'name': 'betrouwbaarheid_interpretatie',
+        'source': 'wfs',
+        'sourcefield': 'Betrouwbaarheid',
+        'type': 'string'
+    }]
+
+    def __init__(self, pkey):
+        """Initialisation.
+
+        Parameters
+        ----------
+        pkey : str
+            Permanent key of the Boring (borehole), being a URI of the form
+            `https://www.dov.vlaanderen.be/data/boring/<id>`.
+
+        """
+        super(HydrogeologischeStratigrafie, self).__init__(
+            'interpretatie', pkey)
+
+    @classmethod
+    def from_wfs_element(cls, feature, namespace):
+        """Build `Boring` instance from a WFS feature element.
+
+        Parameters
+        ----------
+        feature : etree.Element
+            XML element representing a single record of the WFS layer.
+        namespace : str
+            Namespace associated with this WFS featuretype.
+
+        Returns
+        -------
+        boring : Boring
+            An instance of this class populated with the data from the WFS
+            element.
+
+        """
+        infstrat = InformeleStratigrafie(
+            feature.findtext('./{%s}Interpretatiefiche' % namespace))
+
+        typeproef = cls._parse(
+            func=feature.findtext,
+            xpath='Type_proef',
+            namespace=namespace,
+            returntype='string'
+        )
+
+        if typeproef == 'Boring':
+            infstrat.data['pkey_boring'] = cls._parse(
+                func=feature.findtext,
+                xpath='Proeffiche',
+                namespace=namespace,
+                returntype='string'
+            )
+            infstrat.data['pkey_sondering'] = None
+        elif typeproef == 'Sondering':
+            infstrat.data['pkey_sondering'] = cls._parse(
+                func=feature.findtext,
+                xpath='Proeffiche',
+                namespace=namespace,
+                returntype='string'
+            )
+            infstrat.data['pkey_boring'] = None
+        else:
+            infstrat.data['pkey_boring'] = None
+            infstrat.data['pkey_sondering'] = None
+
+        for field in cls.get_fields(source=('wfs',)).values():
+            if field['name'] in ['pkey_boring', 'pkey_sondering']:
+                continue
+
+            infstrat.data[field['name']] = cls._parse(
+                func=feature.findtext,
+                xpath=field['sourcefield'],
+                namespace=namespace,
+                returntype=field.get('type', None)
+            )
+
+        return infstrat
+
+    def _parse_xml_data(self):
+        """Get remote XML data for this DOV object, parse the raw XML and
+        save the results in the data object.
+        """
+        xml = self._get_xml_data()
+        tree = etree.fromstring(xml)
+
+        for field in self.get_fields(source=('xml',),
+                                     include_subtypes=False).values():
+            self.data[field['name']] = self._parse(
+                func=tree.findtext,
+                xpath=field['sourcefield'],
+                namespace=None,
+                returntype=field.get('type', None)
+            )
+
+        self._parse_subtypes(xml)

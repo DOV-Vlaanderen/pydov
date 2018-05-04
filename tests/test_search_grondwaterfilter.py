32f69883aa5c5f2a86ddd7abf93f99628de45c0c
stijnvanhoey <stijnvanhoey@gmail.com>
2018-05-04 11:04:46 +0200

    Add template for filter specific search tests

---
+++ tests/test_search_grondwaterfilter.py
@@ -0,0 +1,12 @@
+"""Module grouping tests for the search grondwaterfilter module."""
+
+
+import pydov
+from pydov.search.grondwaterfilter import GrondwaterFilterSearch
+from pydov.types.grondwaterfilter import GrondwaterFilter
+
+class TestGrondwaterFilterSearch(object):
+    """Class grouping tests for the pydov.search.GrondwaterFilterSearch class.
+    """
+
+    pass

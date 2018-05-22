5c7c0023f422c86d323db99e626aa448e5e35a30
pieterjan <pieterjan.haest@agt.be>
2018-05-22 14:23:01 +0200

    adjust xml string encoding to 'latin-1', because 'utf-8' still yields errors

--- pydov/types/abstract.py
+++ pydov/types/abstract.py
@@ -56,7 +56,7 @@ class AbstractCommon(object):
         """
         if returntype == 'string':
             def typeconvert(x):
-                return str(x.encode("utf-8") ).strip()
+                return str(x.encode('latin-1') ).strip()
         elif returntype == 'integer':
             def typeconvert(x):
                 return int(x)

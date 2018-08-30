491ca7cf078b0ffcf081503927c17cec3f14f5cb
stijnvanhoey <stijnvanhoey@gmail.com>
2018-08-30 15:23:42 +0200

    Move doc installs to dev

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 with open('requirements_dev.txt') as f:
     requirements_dev = f.read().splitlines()
-with open('requirements_doc.txt') as f:
-    requirements_doc = f.read().splitlines()
 
 setup(
     name='pydov',
@@ -49,8 +47,5 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
-    tests_require=requirements_dev + requirements_doc,
-    extras_require={
-        'docs': requirements_doc
-    }
+    tests_require=requirements_dev
 )

d85ca5d643bb48d371402229480b7e8a0eed6c52
stijnvanhoey <stijnvanhoey@gmail.com>
2018-08-30 12:33:07 +0200

    Fix path on static css file

--- docs/conf.py
+++ docs/conf.py
@@ -146,7 +146,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['_static', '_static/custom.css']
+html_static_path = ['_static', '_static/css/custom.css']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.

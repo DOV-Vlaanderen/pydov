345bba53fc569fc727548d78d2e907573285ee08
stijnvanhoey <stijnvanhoey@gmail.com>
2018-08-30 12:32:24 +0200

    Exclude ipynb checkpoints from sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ autoclass_content = 'both'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.

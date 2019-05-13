b63da3cf409c55b11f4119a6747177ea8d453c9c
stijnvanhoey <stijnvanhoey@gmail.com>
2019-05-13 12:09:50 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('requirements_doc.txt') as f:
 
 setup(
     name='pydov',
-    version='0.1.1',
+    version='0.1.2',
     description="A Python package to download data from Databank Ondergrond Vlaanderen (DOV).",
     long_description=readme,
     long_description_content_type='text/markdown',

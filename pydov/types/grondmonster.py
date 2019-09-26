8637c71b1900365229d4436f083f5b8d907283f6
pjhaest <geo.haest@gmail.com>
2019-09-26 12:34:29 +0200

    changes Grondmonster Type to new abstract class and add test files

--- pydov/types/grondmonster.py
+++ pydov/types/grondmonster.py
@@ -1,172 +1,137 @@
 # -*- coding: utf-8 -*-
 """Module containing the DOV data type for screens (Filter), including
 subtypes."""
-import numpy as np
 from .abstract import (
     AbstractDovType,
     AbstractDovSubType,
+)
+from pydov.types.fields import (
+    WfsField,
+    _CustomField,
+    XmlField,
+    XsdType,
 )
 
 
 class Korrelverdeling(AbstractDovSubType):
 
-    _name = 'korrelverdeling'
-    _rootpath = './/grondmonster/observatieReeksData/' \
+    rootpath = './/grondmonster/observatieReeksData/' \
                 'korrelverdeling_reeks/korrelverdeling'
 
-    _fields = [{
-        'name': 'diameter',
-        'source': 'xml',
-        'sourcefield': '/diameter',
-        'definition': '.',
-        'type': 'float',
-        'notnull': False
-    }, {
-        'name': 'fractie',
-        'source': 'xml',
-        'sourcefield': '/fractie',
-        'definition': '.',
-        'type': 'float',
-        'notnull': False
-    }, {
-        'name': 'methode',
-        'source': 'xml',
-        'sourcefield': '/methode',
-        'definition': '.',
-        'type': 'string',
-        'notnull': False
-    }, ]
+    fields =[
+        XmlField(name='diameter',
+                 source_xpath='/diameter',
+                 definition='.',
+                 datatype='float',
+                 notnull=False),
+        XmlField(name='fractie',
+                 source_xpath='/fractie',
+                 definition='.',
+                 datatype='float',
+                 notnull=False),
+        XmlField(name='methode',
+                 source_xpath='/methode',
+                 definition='.',
+                 datatype='string',
+                 notnull=False)
+    ]
 
 
 class Grondmonster(AbstractDovType):
     """Class representing the DOV data type for ground samples."""
 
-    _subtypes = [Korrelverdeling]
+    subtypes = [Korrelverdeling]
 
-    _xsd_schemas = [
-        'https://www.dov.vlaanderen.be/xdov/schema/latest/xsd/kern/'
-        'grondmonster/GrondmonsterDataCodes.xsd',
-    ]
+    __grondmonsterDataCodesEnumType = XsdType(
+        xsd_schema='https://www.dov.vlaanderen.be/xdov/schema/latest/'
+                   'xsd/kern/grondmonster/GrondmonsterDataCodes.xsd',
+        typename='MonsterEnumType'
+    )
+    __grondmonsterParameterEnumType = XsdType(
+        xsd_schema='https://www.dov.vlaanderen.be/xdov/schema/latest/'
+                   'xsd/kern/grondmonster/GrondmonsterDataTypes.xsd',
+        typename='ParameterEnumType'
+    )
 
-    _fields = [{
-        'name': 'pkey_boring',
-        'source': 'wfs',
-        'sourcefield': 'boringfiche',
-        'type': 'string'
-    }, {
-        'name': 'pkey_grondmonster',
-        'source': 'wfs',
-        'sourcefield': 'grondmonsterfiche',
-        'type': 'string'
-    }, {
-        'name': 'naam',
-        'source': 'wfs',
-        'sourcefield': 'naam',
-        'type': 'string'
-    }, {
-        'name': 'boornummer',
-        'source': 'wfs',
-        'sourcefield': 'boornummer',
-        'type': 'string'
-    }, {
-        'name': 'x',
-        'source': 'wfs',
-        'sourcefield': 'X_mL72',
-        'type': 'float'
-    }, {
-        'name': 'y',
-        'source': 'wfs',
-        'sourcefield': 'Y_mL72',
-        'type': 'float'
-    }, {
-        'name': 'z_mtaw',
-        'source': 'wfs',
-        'sourcefield': 'Z_mTAW',
-        'type': 'float'
-    }, {
-        'name': 'gemeente',
-        'source': 'wfs',
-        'sourcefield': 'gemeente',
-        'type': 'string'
-    }, {
-        'name': 'diepte_van_m',
-        'source': 'wfs',
-        'sourcefield': 'diepte_van_m',
-        'type': 'float',
-    }, {
-        'name': 'diepte_tot_m',
-        'source': 'wfs',
-        'sourcefield': 'diepte_tot_m',
-        'type': 'float',
-    }, {
-        'name': 'peil_tot_mtaw',
-        'source': 'wfs',
-        'sourcefield': 'peil_tot_mTAW',
-        'type': 'float',
-    }, {
-        'name': 'monstertype',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/monstertype',
-        'xsd_type': 'MonsterEnumType',
-        'definition': 'type monster',
-        'type': 'string',
-    }, {
-        'name': 'astm_naam',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/observatieData/observatie['
+    fields = [
+        WfsField(name='pkey_boring',
+                 source_field='boringfiche',
+                 datatype='string'),
+        WfsField(name='naam',
+                 source_field='naam',
+                 datatype='string'),
+        WfsField(name='boornummer',
+                 source_field='wfs',
+                 datatype='string'),
+        WfsField(name='x',
+                 source_field='X_mL72',
+                 datatype='float'),
+        WfsField(name='y',
+                 source_field='Y_mL72',
+                 datatype='float'),
+        WfsField(name='z_mtaw',
+                 source_field='Z_mTAW',
+                 datatype='float'),
+        WfsField(name='gemeente',
+                 source_field='gemeente',
+                 datatype='string'),
+        WfsField(name='diepte_van_m',
+                 source_field='diepte_van_m',
+                 datatype='float'),
+        WfsField(name='diepte_tot_m',
+                 source_field='diepte_tot_m',
+                 datatype='float'),
+        WfsField(name='peil_tot_mtaw',
+                 source_field='peil_tot_mTAW',
+                 datatype='float'),
+        XmlField(name='monstertype',
+                 source_xpath='/grondmonster/monstertype',
+                 xsd_type=__grondmonsterDataCodesEnumType,
+                 definition='type monster',
+                 datatype='string'),
+        XmlField(name='astm_naam',
+                 source_xpath='/grondmonster/observatieData/observatie['
                        'parameter="ASTM_NAAM"]/waarde_text',
-        'xsd_type': 'ParameterEnumType',
-        'definition': 'ASTM_naam',
-        'type': 'string'
-    }, {
-        'name': 'grondsoort_bggg',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/observatieData/observatie['
+                 xsd_type=__grondmonsterParameterEnumType,
+                 definition='ASTM_naam',
+                 datatype='string'),
+        XmlField(name='grondsoort_bggg',
+                 source_xpath='/grondmonster/observatieData/observatie['
                        'parameter="BGGG"]/waarde_text',
-        'xsd_type': 'ParameterEnumType',
-        'definition': 'Grondsoort BGGG',
-        'type': 'string'
-    }, {
-        'name': 'humusgehalte',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/observatieData/observatie['
+                 xsd_type=__grondmonsterParameterEnumType,
+                 definition='Grondsoort BGGG',
+                 datatype='string'),
+        XmlField(name='humusgehalte',
+                 source_xpath='/grondmonster/observatieData/observatie['
                        'parameter="HUMUSGEHALTE"]/waarde_numeriek',
-        'xsd_type': 'ParameterEnumType',
-        'definition': 'Humusgehalte',
-        'type': 'float'
-    }, {
-        'name': 'kalkgehalte',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/observatieData/observatie['
+                 xsd_type=__grondmonsterParameterEnumType,
+                 definition='Humusgehalte',
+                 datatype='float'),
+        XmlField(name='kalkgehalte',
+                 source_xpath='/grondmonster/observatieData/observatie['
                        'parameter="KALKGEHALTE"]/waarde_numeriek',
-        'xsd_type': 'ParameterEnumType',
-        'definition': 'Kalkgehalte',
-        'type': 'float'
-    }, {
-        'name': 'uitrolgrens',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/observatieData/observatie['
+                 xsd_type=__grondmonsterParameterEnumType,
+                 definition='Kalkgehalte',
+                 datatype='float'),
+        XmlField(name='uitrolgrens',
+                 source_xpath='/grondmonster/observatieData/observatie['
                        'parameter="UITROLGRENS"]/waarde_numeriek',
-        'xsd_type': 'ParameterEnumType',
-        'definition': 'Uitrolgrens',
-        'type': 'float'
-    }, {
-        'name': 'vloeigrens',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/observatieData/observatie['
+                 xsd_type=__grondmonsterParameterEnumType,
+                 definition='Uitrolgrens',
+                 datatype='float'),
+        XmlField(name='vloeigrens',
+                 source_xpath='/grondmonster/observatieData/observatie['
                        'parameter="VLOEIGRENS"]/waarde_numeriek',
-        'xsd_type': 'ParameterEnumType',
-        'definition': 'Vloeigrens',
-        'type': 'float'
-    }, {
-        'name': 'glauconiet',
-        'source': 'xml',
-        'sourcefield': '/grondmonster/observatieData/observatie['
+                 xsd_type=__grondmonsterParameterEnumType,
+                 definition='Vloeigrens',
+                 datatype='float'),
+        XmlField(name='glauconiet',
+                 source_xpath='/grondmonster/observatieData/observatie['
                        'parameter="GLAUCONIET_TOTAAL"]/waarde_numeriek',
-        'xsd_type': 'ParameterEnumType',
-        'definition': 'Glauconiet totaal',
-        'type': 'float'
-    }]
+                 xsd_type=__grondmonsterParameterEnumType,
+                 definition='Glauconiet totaal',
+                 datatype='float'),
+    ]
 
     def __init__(self, pkey):
         """Initialisation.

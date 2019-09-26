c6baf76ed29609830223f3b1762c6a57f02753e1
pjhaest <geo.haest@gmail.com>
2019-09-26 15:38:56 +0200

    first tests

---
+++ tests/test_search_grondmonster.py
@@ -0,0 +1,199 @@
+"""Module grouping tests for the search grondwaterfilter module."""
+import datetime
+
+from owslib.fes import PropertyIsEqualTo
+from pydov.search.grondmonster import GrondmonsterSearch
+from pydov.types.grondmonster import Grondmonster
+from tests.abstract import (
+    AbstractTestSearch,
+)
+
+from tests.test_search import (
+    mp_wfs,
+    wfs,
+    mp_remote_md,
+    mp_remote_fc,
+    mp_remote_describefeaturetype,
+    mp_remote_wfs_feature,
+    mp_remote_xsd,
+    mp_dov_xml,
+    mp_dov_xml_broken,
+    wfs_getfeature,
+    wfs_feature,
+)
+
+location_md_metadata = 'tests/data/types/grondmonster/md_metadata.xml'
+location_fc_featurecatalogue = \
+    'tests/data/types/grondmonster/fc_featurecatalogue.xml'
+location_wfs_describefeaturetype = \
+    'tests/data/types/grondmonster/wfsdescribefeaturetype.xml'
+location_wfs_getfeature = 'tests/data/types/grondmonster/wfsgetfeature.xml'
+location_wfs_feature = 'tests/data/types/grondmonster/feature.xml'
+location_dov_xml = 'tests/data/types/grondmonster/grondwaterfilter.xml'
+location_xsd_base = 'tests/data/types/grondmonster/xsd_*.xml'
+
+
+class TestGrondmonsterSearch(AbstractTestSearch):
+    def get_search_object(self):
+        """Get an instance of the search object for this type.
+
+        Returns
+        -------
+        pydov.search.grondmonster.GrondmonsterSearch
+            Instance of GrondmonsterSearch used for searching.
+
+        """
+        return GrondmonsterSearch()
+
+    def get_type(self):
+        """Get the class reference for this datatype.
+
+        Returns
+        -------
+        pydov.types.grondmonster.Grondmonster
+            Class reference for the Grondmonster class.
+
+        """
+        return Grondmonster
+
+    def get_valid_query_single(self):
+        """Get a valid query returning a single feature.
+
+        Returns
+        -------
+        owslib.fes.OgcExpression
+            OGC expression of the query.
+
+        """
+        return PropertyIsEqualTo(propertyname='boornummer',
+                                 literal='GEO-04/024-B6')
+
+    def get_inexistent_field(self):
+        """Get the name of a field that doesn't exist.
+
+        Returns
+        -------
+        str
+            The name of an inexistent field.
+
+        """
+        return 'onbestaand'
+
+    def get_xml_field(self):
+        """Get the name of a field defined in XML only.
+
+        Returns
+        -------
+        str
+            The name of the XML field.
+
+        """
+        return 'monstertype'
+
+    def get_valid_returnfields(self):
+        """Get a list of valid return fields from the main type.
+
+        Returns
+        -------
+        tuple
+            A tuple containing only valid return fields.
+
+        """
+        return ('pkey_grondmonster', 'boornummer')
+
+    def get_valid_returnfields_subtype(self):
+        """Get a list of valid return fields, including fields from a subtype.
+
+        Returns
+        -------
+        tuple
+            A tuple containing valid return fields, including fields from a
+            subtype.
+
+        """
+        return ('pkey_grondmonster', 'boornummer', 'diameter')
+
+    def get_valid_returnfields_extra(self):
+        """Get a list of valid return fields, including extra WFS only
+        fields not present in the default dataframe.
+
+        Returns
+        -------
+        tuple
+            A tuple containing valid return fields, including extra fields
+            from WFS, not present in the default dataframe.
+
+        """
+        return ('pkey_grondmonster', 'korrelverdeling')
+
+    def get_df_default_columns(self):
+        """Get a list of the column names (and order) from the default
+        dataframe.
+
+        Returns
+        -------
+        list
+            A list of the column names of the default dataframe.
+
+        """
+        return ['pkey_grondmonster', 'naam', 'boornummer',
+                'x', 'y', 'z_mtaw',
+                'gemeente', 'diepte_van_m', 'diepte_tot_m',
+                'peil_tot_mtaw', 'monstertype',
+                'astm_naam', 'grondsoort_bggg',
+                'humusgehalte', 'kalkgehalte', 'uitrolgrens',
+                'vloeigrens', 'glauconiet',]
+
+    def test_search_date(self, mp_wfs, mp_remote_describefeaturetype,
+                         mp_remote_md, mp_remote_fc, mp_remote_wfs_feature,
+                         mp_dov_xml):
+        """Test the search method with only the query parameter.
+
+        Test whether the result is correct.
+
+        Parameters
+        ----------
+        mp_wfs : pytest.fixture
+            Monkeypatch the call to the remote GetCapabilities request.
+        mp_remote_describefeaturetype : pytest.fixture
+            Monkeypatch the call to a remote DescribeFeatureType.
+        mp_remote_md : pytest.fixture
+            Monkeypatch the call to get the remote metadata.
+        mp_remote_fc : pytest.fixture
+            Monkeypatch the call to get the remote feature catalogue.
+        mp_remote_wfs_feature : pytest.fixture
+            Monkeypatch the call to get WFS features.
+        mp_dov_xml : pytest.fixture
+            Monkeypatch the call to get the remote XML data.
+
+        """
+        df = self.get_search_object().search(
+            query=self.get_valid_query_single())
+
+        # specific test for the Zulu time wfs 1.1.0 issue
+        assert df.datum.sort_values()[0] == datetime.date(2004, 4, 7)
+
+    def test_search_xmlresolving(self, mp_remote_describefeaturetype,
+                                 mp_remote_wfs_feature, mp_dov_xml):
+        """Test the search method with return fields from XML but not from a
+        subtype.
+
+        Test whether the output dataframe contains the resolved XML data.
+
+        Parameters
+        ----------
+        mp_remote_describefeaturetype : pytest.fixture
+            Monkeypatch the call to a remote DescribeFeatureType.
+        mp_remote_wfs_feature : pytest.fixture
+            Monkeypatch the call to get WFS features.
+        mp_dov_xml : pytest.fixture
+            Monkeypatch the call to get the remote XML data.
+
+        """
+        df = self.get_search_object().search(
+            query=self.get_valid_query_single(),
+            return_fields=('pkey_filter', 'gw_id', 'filternummer',
+                           'meetnet_code'))
+
+        assert df.humusgehalte[0] == 15.6
+        assert df.methode[142] == 'AREOMETER'

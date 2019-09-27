b50436fa17a79cbfec19ada500213808f4725f8f
Roel Huybrechts <roel.huybrechts@realdolmen.com>
2019-09-27 14:30:41 +0200

    Fix docstrings.

--- pydov/search/interpretaties.py
+++ pydov/search/interpretaties.py
@@ -13,7 +13,7 @@ from pydov.util import owsutil
 
 
 class InformeleStratigrafieSearch(AbstractSearch):
-    """Search class to retrieve information about boreholes (Boring)."""
+    """Search class to retrieve information about 'informele stratigrafie'."""
 
     __wfs_schema = None
     __wfs_namespace = None
@@ -82,7 +82,7 @@ class InformeleStratigrafieSearch(AbstractSearch):
 
     def search(self, location=None, query=None, return_fields=None,
                max_features=None):
-        """Search for boreholes (Boring). Provide either `location` or `query`.
+        """Search for 'informele stratigrafie'. Provide either `location` or `query`.
         When `return_fields` is None, all fields are returned.
 
         Parameters
@@ -218,8 +218,8 @@ class FormeleStratigrafieSearch(AbstractSearch):
 
     def search(self, location=None, query=None, return_fields=None,
                max_features=None):
-        """Search for boreholes (Boring). Provide either `location` or `query`.
-        When `return_fields` is None, all fields are returned.
+        """Search for 'formele stratigrafie'. Provide either `location` or
+        `query`. When `return_fields` is None, all fields are returned.
 
         Parameters
         ----------

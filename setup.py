51478c24cf069c969090b2cb219647c73ab38a80
pieterjan <pieterjan.haest@agt.be>
2020-01-27 10:56:54 +0100

    remove python 2 from setup

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Natural Language :: Dutch',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',

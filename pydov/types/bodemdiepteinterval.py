b3b0337f3bc740e831e468c64563e8362994fc0d
GuillaumeVandekerckhove <guillaume.vandekerckhove@outlook.com>
2021-09-16 10:29:28 +0200

    Adapted code for bodemdiepteinterval (search and types).

---
+++ pydov/types/bodemdiepteinterval.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""Module containing the DOV data type for bodemdiepteintervallen, including
+subtypes."""
+from pydov.types.fields import WfsField, XmlField
+
+from .abstract import AbstractDovType
+
+
+class Bodemdiepteinterval(AbstractDovType):
+    """Class representing the DOV data type for bodemdiepteinterval."""
+
+    subtypes = []
+
+    fields = [
+        WfsField(name='nr',
+                 source_field='Nr',
+                 datatype='float'),
+        WfsField(name='type',
+                 source_field='Type',
+                 datatype='string'),
+        WfsField(name='pkey_diepteinterval',
+                 source_field='Diepteintervalfiche',
+                 datatype='string'),
+        WfsField(name='pkey_bodemopbouw',
+                 source_field='Bodemopbouwfiche',
+                 datatype='string'),
+        WfsField(name='naam',
+                 source_field='Naam',
+                 datatype='string'),
+        WfsField(name='bovengrens 1 (cm)',
+                 source_field='Bovengrens1',
+                 datatype='float'),
+        WfsField(name='bovengrens 2 (cm)',
+                 source_field='Bovengrens2',
+                 datatype='float'),
+        WfsField(name='bovengrenzen (cm)',
+                 source_field='Bovengrenzen',
+                 datatype='string'),
+        WfsField(name='ondergrens 1 (cm)',
+                 source_field='Ondergrens1',
+                 datatype='float'),
+        WfsField(name='ondergrens 2 (cm)',
+                 source_field='Ondergrens2',
+                 datatype='float'),
+        WfsField(name='ondergrenzen (cm)',
+                 source_field='Ondergrenzen',
+                 datatype='string'),
+        WfsField(name='ondergrens bereikt',
+                 source_field='Ondergrens_bereikt',
+                 datatype='string'),
+        WfsField(name='grensduidelijkheid',
+                 source_field='Grensduidelijkheid',
+                 datatype='string'),
+        WfsField(name='grensregelmatigheid',
+                 source_field='Grensregelmatigheid',
+                 datatype='string'),
+        WfsField(name='beschrijving',
+                 source_field='Beschrijving',
+                 datatype='string'),
+        WfsField(name='monsters',
+                 source_field='Monsters',
+                 datatype='string'),
+        WfsField(name='monsterlijst',
+                 source_field='Monsterlijst',
+                 datatype='string'),
+        WfsField(name='observaties',
+                 source_field='Observaties',
+                 datatype='string'),
+        WfsField(name='observatielijst',
+                 source_field='Observatielijst',
+                 datatype='string'),
+        WfsField(name='opmerkingen',
+                 source_field='Opmerkingen',
+                 datatype='string'),
+        WfsField(name='opmerkinglijst',
+                 source_field='Opmerkinglijst',
+                 datatype='string'),
+        WfsField(name='bodemlocatie',
+                 source_field='Bodemlocatie',
+                 datatype='string'),
+        WfsField(name='pkey_bodemlocatie',
+                 source_field='Bodemlocatiefiche',
+                 datatype='string'),
+        WfsField(name='x',
+                 source_field='X_mL72',
+                 datatype='float'),
+        WfsField(name='y',
+                 source_field='Y_mL72',
+                 datatype='float'),
+        WfsField(name='mv_mtaw',
+                 source_field='mv_mTAW',
+                 datatype='float'),
+    ]
+
+    pkey_fieldname = 'Diepteintervalfiche'
+
+    def __init__(self, pkey):
+        """Initialisation.
+
+        Parameters
+        ----------
+        pkey : str
+            Permanent key of the Bodemdiepteinterval, being a URI of the form
+            `https://www.dov.vlaanderen.be/data/bodemdiepteinterval/<id>`.
+
+        """
+        super().__init__('Bodemdiepteinterval', pkey)

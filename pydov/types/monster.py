406bbdbe3bbc227559780c1dc3f61dd03974ba7c
arnedegr <151182706+arnedegr@users.noreply.github.com>
2025-06-06 10:53:04 +0200

    Add subtype Opslaglocatie for Monster type (#417)
    * DOVDATA-922 code + documentatie
    
    * DOVDATA-922 opslaglocatie: correcting typo + flake8
    
    * DOVDATA-922
    
    * Some documentation fixes.
    
    ---------
    
    Co-authored-by: ArneDegrave <arne.degrave@geosolutions.be>
    Co-authored-by: Roel Huybrechts <roel@huybrechts.re>

--- pydov/types/monster.py
+++ pydov/types/monster.py
@@ -31,6 +31,30 @@ class BemonsterdObject(AbstractDovSubType):
     ]
 
 
+class Opslaglocatie(AbstractDovSubType):
+    """Subtype listing the storage location(s) of the sample."""
+
+    rootpath = './/monster/opslaglocatie'
+    intended_for = ['Monster']
+
+    fields = [
+        XmlField(name='opslaglocatie_naam',
+                 source_xpath='/locatie',
+                 definition='Oplaglocatie',
+                 datatype='string'),
+        XmlField(name='opslaglocatie_van',
+                 source_xpath='/beschikbaar_vanaf',
+                 definition='Startdatum van de opslag van '
+                            'het monster op die locatie.',
+                 datatype='string'),
+        XmlField(name='opslaglocatie_tot',
+                 source_xpath='/beschikbaar_tot',
+                 definition='Einddatum van de opslag '
+                            'van het monster op die locatie.',
+                 datatype='string')
+    ]
+
+
 class Monster(AbstractDovType):
     """Class representing the DOV data type for ground samples."""
 

579230f9c154834853227ac0523e3abc207b1914
Roel Huybrechts <roel@huybrechts.re>
2025-08-01 09:50:13 +0200

    Add subtype Meetreeks for Observatie type (#425)
    * Initial work on Fractiemeting.
    
    * Add unit test.
    
    * Update documentation.
    
    * Update docs
    
    * Omit the fields resultaat and detectieconditie by default in ObservatieFractiemetingSearch, since these will always be empty in this case.
    
    * Add Meetreeks subtype for Observatie and introduce ObservatieMeetreeksSearch
    
    * Remove duplicate testfile

--- pydov/types/observatie.py
+++ pydov/types/observatie.py
@@ -1,10 +1,76 @@
 # -*- coding: utf-8 -*-
 """Module containing the DOV data type for observations (Observatie), including
 subtypes."""
-from pydov.types.fields import WfsField, XmlField
+from collections import OrderedDict
+
+import numpy as np
+
+from pydov.types.fields import WfsField, XmlField, _CustomXmlField
 from pydov.types.fields_custom import OsloCodeListValueField
 
 from .abstract import AbstractDovType, AbstractDovSubType, AbstractDovFieldSet
+
+
+class NumeriekTekstField(_CustomXmlField):
+    """Field for retrieving a numeric or text value from the XML, depending on
+    which is available. This is used for fields that can have either a numeric
+    or text representation."""
+
+    def __init__(self, name, definition, basename):
+        """Initialise a NumeriekTekstField with given definition and basename.
+
+        Parameters
+        ----------
+        name : string
+            Name of the field, used in the resulting dataframe.
+        definition : string
+            Type-specific definition of the meetwaarde field.
+        basename : string
+            The basename of the field, which is used to find the correct XML
+            elements in the DOV XML. The field will look for
+            `<basename>_numeriek` and `<basename>_text` in the XML to
+            retrieve the value.
+        """
+        super().__init__(
+            name=name,
+            definition=definition,
+            datatype='string',
+            notnull=False
+        )
+
+        self.basename = basename
+
+    def calculate(self, cls, tree):
+        """Calculate the value of the measurement.
+
+        Parameters
+        ----------
+        tree : etree.ElementTree
+            ElementTree of the DOV XML for this instance.
+
+        Returns
+        -------
+        str
+            Value of field, parsed from either <basename>_numeriek or
+            <basename>_text.
+        """
+
+        waarde_numeriek = cls._parse(
+            func=tree.findtext,
+            xpath=f'.//{self.basename}_numeriek',
+            namespace=None,
+            returntype='string'
+        )
+        if waarde_numeriek is not np.nan:
+            return waarde_numeriek
+
+        waarde_text = cls._parse(
+            func=tree.findtext,
+            xpath=f'.//{self.basename}_text',
+            namespace=None,
+            returntype='string'
+        )
+        return waarde_text
 
 
 class ObservatieDetails(AbstractDovFieldSet):
@@ -80,6 +146,83 @@ class Fractiemeting(AbstractDovSubType):
                  definition='Gemeten massaprocent van de fractie',
                  datatype='float')
     ]
+
+
+class MeetreeksWaarde(AbstractDovSubType):
+    """Subtype showing the details of a measurement value in a series."""
+
+    rootpath = './/meetreekswaarde'
+    intended_for = ['Meetreeks']
+
+    fields = [
+        NumeriekTekstField(name='meetreeks_meetpunt',
+                           definition='Meetpunt',
+                           basename='meetpunt'),
+        NumeriekTekstField(name='meetreeks_meetwaarde',
+                           definition='Meetwaarde',
+                           basename='meetwaarde')
+    ]
+
+
+class Meetreeks(AbstractDovSubType):
+    """Subtype showing the details of a measurement series."""
+
+    rootpath = './/waarde_meetreeks'
+    intended_for = ['Observatie']
+
+    subtypes = [MeetreeksWaarde]
+
+    fields = [
+        XmlField(name='meetreeks_meetpunt_parameter',
+                 source_xpath='/meetpuntparameter',
+                 definition=('(Verkorte) naam van de parameter voor het '
+                             'meetpunt'),
+                 datatype='string'),
+        XmlField(name='meetreeks_meetpunt_eenheid',
+                 source_xpath='/meetpuntparameter_eenheid',
+                 definition='Eenheid van de meetpuntparameter',
+                 datatype='string'),
+        XmlField(name='meetreeks_meetwaarde_parameter',
+                 source_xpath='/meetwaardeparameter',
+                 definition=('(Verkorte) naam van de parameter voor de '
+                             'meetwaarde',),
+                 datatype='string'),
+        XmlField(name='meetreeks_meetwaarde_eenheid',
+                 source_xpath='/meetwaardeparameter_eenheid',
+                 definition='Eenheid van de meetwaardeparameter',
+                 datatype='string')
+    ]
+
+    _preferred_field_order = [
+        'meetreeks_meetpunt_parameter', 'meetreeks_meetpunt',
+        'meetreeks_meetpunt_eenheid', 'meetreeks_meetwaarde_parameter',
+        'meetreeks_meetwaarde', 'meetreeks_meetwaarde_eenheid'
+    ]
+
+    @classmethod
+    def get_fields(cls):
+        """Get the fields of the Meetreeks subtype.
+
+        Returns
+        -------
+        list
+            List of fields defined in this subtype.
+        """
+        return OrderedDict(sorted(
+            super().get_fields().items(),
+            key=lambda item: cls._preferred_field_order.index(item[0])))
+
+    @classmethod
+    def get_field_names(cls):
+        """Get the field names of the Meetreeks subtype.
+
+        Returns
+        -------
+        list
+            List of field names defined in this subtype.
+        """
+        return sorted(super().get_field_names(),
+                      key=Meetreeks._preferred_field_order.index)
 
 
 class Observatie(AbstractDovType):
